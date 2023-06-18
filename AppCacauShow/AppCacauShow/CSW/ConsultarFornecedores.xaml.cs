142364a0e37358bdb58cb5183e92ff612961acf9
ThauanyCelestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-06-18 19:18:54 -0400

    CONSULTAR FORNECDOR

---
+++ AppCacauShow/AppCacauShow/CSW/ConsultarFornecedores.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AppCacauShow.CSW
+{
+    /// <summary>
+    /// Lógica interna para ConsultarFornecedores.xaml
+    /// </summary>
+    public partial class ConsultarFornecedores : Window
+    {
+        public ConsultarFornecedores()
+        {
+            InitializeComponent();
+        }
+    }
+}
